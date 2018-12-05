f66c7ee30dc35090ac58cba0d71e66fbda076486
Nguyen-Cong-Nghiem <40155344+Nguyen-Cong-Nghiem@users.noreply.github.com>
2018-12-05 10:05:16 +0700

    Add class DataSync

---
+++ src/DataSync.java
@@ -0,0 +1,18 @@
+public class DataSync {
+    private int id;
+    private long simulationDuration;
+
+    public DataSync(int id, long simulationDuration) {
+        this.id = id;
+        this.simulationDuration = simulationDuration;
+    }
+
+    public String get() {
+        try {
+            Thread.sleep(this.simulationDuration);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "data" + id;
+    }
+}
